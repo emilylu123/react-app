d2763ad2a311293ab9d01d8134c7b7d0158420ee
Yingyao Lu <emilylu123@gmail.com>
2021-08-23 15:00:09 +0930

    add search box using filter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component'
-
+import SearchBox from './components/search-box/search.component';
 
 class App extends Component {
+
   constructor() {
     super();
     this.state = {
       Header: "Hello Star War",
       starWars: [],
+      searchField: ''
     };
+  }
+  handleSearch() {
+    console.log("Search for: " + this.searchField)
   }
 
   componentDidMount() {
@@ -19,17 +24,27 @@ class App extends Component {
       .then(res => this.setState({ starWars: res.results }));
 
   }
-  componentDidUpdate() {
-    console.log("componentDidUpdate")
-    console.log(this.state.starWars)
-  }
+  // componentDidUpdate() {
+  //   console.log("componentDidUpdate")
+  //   console.log(this.state.starWars)
+  // }
 
   render() {
+    const { starWars, searchField } = this.state;
+    const filteredStar = starWars.filter(star => {
+      return star.name.toLowerCase().includes(searchField.toLowerCase())
+    })
+
     return (
-      <div className='App'>
+      <div className='App' >
         <h1>{this.state.Header}</h1>
-        <CardList stars={this.state.starWars} />
-      </div>
+        <SearchBox placeholder="search stars" handleChange={e => {
+          this.setState({ searchField: e.target.value },
+          );
+        }} />
+
+        <CardList stars={filteredStar} />
+      </div >
     );
   }
 }
